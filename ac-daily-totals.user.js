f523e83c02443ff00f2d29ea0fdbb14a398d2b4b
Eric Bright <eric.bright@pixelunion.net>
2017-07-23 13:39:32 +0100

    Timer performance
    - Slow down timer checking
    - Require jQuery

--- ac-daily-totals.user.js
+++ ac-daily-totals.user.js
@@ -5,35 +5,39 @@
 // @description     Calculates daily time totals for Active Collab
 // @downloadURL     https://github.com/ericbdev/tm-ac-daily-totals/raw/master/ac-daily-totals.js
 // @include         http*://app.activecollab.com/*/my-time
+// @require         https://code.jquery.com/jquery-3.2.1.slim.min.js
 // ==/UserScript==
 
+function leftPad(number) {
+  return (number < 10 && number >= 0 ? '0' : '') + number;
+}
+
+function sum(array) {
+  return array.reduce(function (a, b) {
+    return a + b;
+  });
+}
+
+function timePerTasks($timeValues) {
+  var hoursWorked = [];
+  var minutesWorked = [];
+
+  $timeValues.each(function (key, value) {
+    // Convert string to minutes
+    var time = $(value).text();
+    var timeChunks = time.split(':');
+
+    minutesWorked.push(parseInt(timeChunks[timeChunks.length - 1], 10));
+    hoursWorked.push(parseInt(timeChunks[timeChunks.length - 2], 10));
+  });
+
+  return {
+    hoursWorked: hoursWorked,
+    minutesWorked: minutesWorked,
+  };
+}
+
 function timeTrackedPerDay() {
-  function leftPad(number) {
-    return (number < 10 && number >= 0 ? '0' : '') + number;
-  }
-
-  function sum(array) {
-    return array.reduce(function (a, b) {
-      return a + b;
-    });
-  }
-
-  function timePerTasks($timeValues) {
-    var hoursWorked = [];
-    var minutesWorked = [];
-
-    $timeValues.each(function (key, value) {
-      // Convert string to minutes
-      var time = $(value).text();
-      var timeChunks = time.split(':');
-
-      minutesWorked.push(parseInt(timeChunks[timeChunks.length - 1], 10));
-      hoursWorked.push(parseInt(timeChunks[timeChunks.length - 2], 10));
-    });
-
-    return { hoursWorked: hoursWorked, minutesWorked: minutesWorked };
-  }
-
   var $timeGroups = $('.tracking_objects_list_group');
 
   if ($timeGroups.length) {
@@ -41,6 +45,7 @@ function timeTrackedPerDay() {
       var $group = $(value);
       var $timeValues = $group.find('[data-qa-id="time-record-value"]');
       var $groupHeader = $group.find('[data-qa-id="date-tracking-value"]');
+
       if (!$groupHeader.data('backup-text')) {
         $groupHeader.data('backup-text', $groupHeader.text());
       }
@@ -51,7 +56,6 @@ function timeTrackedPerDay() {
 
       var hoursWorked = _timePerTasks.hoursWorked;
       var minutesWorked = _timePerTasks.minutesWorked;
-
 
       var totalHoursPerDay = sum(hoursWorked);
       var totalMinutesPerDay = sum(minutesWorked);
@@ -65,8 +69,10 @@ function timeTrackedPerDay() {
   }
 }
 
-(function () {
+$(function() {
+  timeTrackedPerDay();
+
   setInterval(function () {
     timeTrackedPerDay();
-  }, 200);
-})();
+  }, 1000);
+});
